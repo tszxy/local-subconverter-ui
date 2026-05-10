c8a8e422307a1fe15901787fcc9e16128e9c987a
Zhang <zhang@ZhangdeMacBook-Pro.local>
2026-05-10 17:52:45 +0800

    Remove obsolete launch helper

--- launchd/start-subconverter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-set -eu
-
-cd /Users/zhang/Documents/Codex/2026-05-10/clash/subconverter
-echo "Starting local subconverter from $(pwd) at $(date)" >> subconverter.launch.log
-exec ./subconverter
